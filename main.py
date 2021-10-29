d9cf603b4b003a38e26cb23bc1553215ced0b82e
reisdev <matheusdrdj@gmail.com>
2021-10-29 18:48:10 -0300

    Implementando encrypt e decrypt

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
 from random import randint
 
 def gcd(a, b):
-    smaller = a if a < b else b
-    for i in range(1, smaller+1):
-        if((a % i == 0) and (b % i == 0)):
-            gcd = i
-    return gcd
+    if(b == 0):
+        return a
+    return gcd(b,a % b)
 
 def gcdExtended(a, b): 
-    if a == 0:  
-        return b,0,1
+    gcd, r = a, b
+    old_s, s = 1, 0
+    old_t, t = 0, 1
+    
+    while r != 0:
+        quotient = gcd // r
+        gcd, r = r, gcd - (quotient * r)
+        old_s, s = s, old_s - (quotient * s)
+        old_t, t = t, old_t - (quotient * t)
 
-    gcd,s1,t1 = gcdExtended(b%a, a) 
+    return gcd,old_s if old_s > 0 else old_s + b,old_t
 
-    s = s1 - (b//a) * t1 
-    t = t1 
+def powMod(a,b,n = 1):
+    if(b == 0):
+        return 1
 
-    return gcd,s if s > 0 else s + b,t
-     
+    if(b % 2 == 0):
+        return (powMod(a, b // 2,n) ** 2) % n
+    else:
+        return (a * (powMod(a, b // 2,n) ** 2)) % n
+
 # Fonte (adaptado): https://github.com/lucasvalentim/pequeno-teorema-de-fermat
-def prime_candidate(n, tentativas = 20):
+def prime_candidate(n, tentativas = 50):
     if n < 1:
         return False
     while tentativas > 0:
-        a = randint(2, 1000)
+        a = randint(2,2**15-1)
 
         while gcd(n, a) != 1:
-            a = randint(2, 1000)
+            a = randint(2,2**15-1)
 
-        if pow(a, n - 1, n) != 1:
+        if powMod(a, n - 1, n) != 1:
             return False
 
         tentativas -= 1
@@ -36,45 +45,106 @@ def prime_candidate(n, tentativas = 20):
     return True
 
 def generate_gcd(phi):
-    e = randint(2, 100000)
+    e = randint(2**14,2**15-1)
 
     while gcd(e,phi) != 1:
-        e = randint(2, 100000)
+        e = randint(2**14,2**15-1)
 
     return e
+
+def getBlockSize(n):
+    block_size = next_size = i = 0
+
+    while True:
+        next_size += 25 * 10**(2*i)
+
+        if next_size < n:
+            block_size = i + 1
+            i+=1
+        else:
+            break
+
+    return block_size
 
 def keygen():
     p,q = 0,0
 
     p_prime,q_prime = False,False
-    while not p_prime or not q_prime:
-        a = randint(2, 100000)
+    while not p_prime:
+        a = randint(2**14, 2**15-1)
         p_prime = prime_candidate(a)
         if p_prime:
             p = a
-
-        a = randint(2, 1000)
-        q_prime = randint(2, 100000)
+    
+    while not q_prime:
+        a = randint(2**14, 2**15-1)
+        q_prime = prime_candidate(a)
         if q_prime:
             q = a
 
+    print(p,q)
     n = p*q
     phi = (p-1)*(q-1)
     e = generate_gcd(phi)
 
     _,d,t = gcdExtended(e,phi)
 
-    print((n,e),d)
+    return (n,e),(n,d) # (public, private)
 
 def encrypt(key,payload):
     (n,e) = key
 
-    # TODO: Implementar criptografia de mensagem
-    C = (payload ** e) % n
+    block_size = getBlockSize(n)
 
+    M = [(ord(c) - 65) for c in payload]
+
+    blocks = [M[start:start+block_size] for start in range(0,len(M),block_size)]
+
+    C = []
+
+    for block in blocks:
+        m_i = 0
+
+        while len(block) % block_size != 0:
+            block.append(25)
+
+        i = block_size - 1
+        for char in block:
+            m_i += (char * 10 ** (2*i))
+            i -= 1
+
+        c_i = powMod(m_i,e,n)
+
+        C.append(c_i)
     return C
 
-if __name__ == "__main__":
-    keygen()
+def decrypt(key,payload):
+    (n,d) = key
 
-    key = (514449163,490925531) # Félix & Aguilar
+    P = []
+    for c_i in payload:
+        P_i = powMod(c_i,d,n)
+
+        P.append(P_i)
+
+    M = []
+
+    block_size = getBlockSize(n)
+
+    for m in P:
+        message = ""
+        
+        aux = m
+        i = block_size
+        while i > 0:
+            c = aux % 100
+            char = chr(c + 65)
+
+            message += char
+
+            aux //= 100
+            i -=1
+        
+        M.append(message[::-1])
+    return "".join(M)
+
